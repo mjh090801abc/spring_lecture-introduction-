bb7f88d54406595d8c6668f60b43e591249a0597
문정훈 <mjh090801abc@dgsw.hs.kr>
2026-01-05 15:56:50 +0900

    mustache에 따라 변수 등록

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,17 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) {
+        // addAttribute로 변수 등록
+        model.addAttribute("username", "문정훈");
+
+        return "greetings"; // 응답 페이지 설정 (브라우저로 전송)
+    }
+}
