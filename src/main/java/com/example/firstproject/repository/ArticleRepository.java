439466ddf9cdf6d921cd35ad5f2fbba3282bd000
문정훈 <mjh090801abc@dgsw.hs.kr>
2026-01-06 20:30:49 +0900

    Article 타입의 ArrayList의 모든 엔티티를 조회할 수 있도록 findAll()사용

--- src/main/java/com/example/firstproject/repository/ArticleRepository.java
+++ src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -3,6 +3,10 @@ package com.example.firstproject.repository;
 import com.example.firstproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> { // <관리 대상 entity, 대표값의 타입>
 
+    @Override
+    ArrayList<Article> findAll();
 }
