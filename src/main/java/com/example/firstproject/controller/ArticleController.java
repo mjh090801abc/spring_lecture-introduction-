f98b54a4f0d7927ba7807cee79e197dc556bad2f
문정훈 <mjh090801abc@dgsw.hs.kr>
2026-01-07 16:04:22 +0900

    업데이트를 페이지로 넘어갈 수 있게 하였고 DTO를 엔티티로 변환하는 코드, 엔티티를 DB로 저장하는 코드 작성, 엔티티가 DB로 갱신하는 코드 추가

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -84,4 +84,25 @@ public class ArticleController {
         // 뷰 페이지 설정
         return "articles/edit";
     }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form){
+        log.info(form.toString());
+
+        // 1: DTO를 엔티티로 변환
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+
+        // 2: 엔티티를 DB로 저장
+        // 2-1: DB에서 기존 데이터를 가져온다
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+
+        // 2-2: 기존 데이터에 값을 갱신
+        if (target != null) {
+            articleRepository.save(articleEntity); // 엔티티가 DB로 갱신
+        }
+
+        // 3: 수정 결과 페이지로 리다이랙트 한다
+        return "redirect:/articles/" + articleEntity.getId();
+    }
 }
