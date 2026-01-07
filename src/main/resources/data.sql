0cc55ceb8f7157cd6ac3c01e0322ed77902f5b51
문정훈 <mjh090801abc@dgsw.hs.kr>
2026-01-07 16:03:27 +0900

    더미 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO article(id, title, content) VALUES (1, '집 가고 싶다', '집 가고 싶다');
-INSERT INTO article(id, title, content) VALUES (2, '집 가고 싶다..', '집 가고 싶다..');
-INSERT INTO article(id, title, content) VALUES (3, '집 가고 싶다...', '집 가고 싶다...');
+insert into article(id, title, content) values (1, '가', '1');
+insert into article(id, title, content) values (2, '나', '2');
+insert into article(id, title, content) values (3, '다', '3');
