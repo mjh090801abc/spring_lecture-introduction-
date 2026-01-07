08662cb3e82a20e1f7954fbe0329b324b5f7932e
문정훈 <mjh090801abc@dgsw.hs.kr>
2026-01-07 16:03:03 +0900

    DTO에도 id필드 추가

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -8,10 +8,11 @@ import lombok.ToString;
 @ToString // toString 메소드
 public class ArticleForm {
 
+    private Long id; // id 필드 추가
     private String title;
     private String content;
 
     public Article toEntity() {
-        return new Article(null, title, content);
+        return new Article(id, title, content);
     }
 }
