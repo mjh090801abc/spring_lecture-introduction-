088b24180a74ac4536444e3116150c3c92b4e38c
문정훈 <mjh090801abc@dgsw.hs.kr>
2026-01-07 13:52:27 +0900

    게시판에 임시 데이터 삽입

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article(id, title, content) VALUES (1, '집 가고 싶다', '집 가고 싶다');
+INSERT INTO article(id, title, content) VALUES (2, '집 가고 싶다..', '집 가고 싶다..');
+INSERT INTO article(id, title, content) VALUES (3, '집 가고 싶다...', '집 가고 싶다...');
