6c7d5c4d26b83a04834554134af209dfc44ab9af
문정훈 <mjh090801abc@dgsw.hs.kr>
2026-01-13 16:18:10 +0900

    더미 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
-insert into article(id, title, content) values (1, '가', '1');
-insert into article(id, title, content) values (2, '나', '2');
-insert into article(id, title, content) values (3, '다', '3');
+INSERT INTO ARTICLE (TITLE, CONTENT) VALUES ('A', 'a'); -- id=1
+INSERT INTO ARTICLE (TITLE, CONTENT) VALUES ('B', 'b'); -- id=2
+INSERT INTO ARTICLE (TITLE, CONTENT) VALUES ('C', 'c'); -- id=3
+
+ALTER TABLE ARTICLE ALTER COLUMN ID RESTART WITH 4;
