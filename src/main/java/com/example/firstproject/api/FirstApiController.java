54d576191c9c408aa4c61b3b9008ef7b5abee4ec
문정훈 <mjh090801abc@dgsw.hs.kr>
2026-01-15 09:39:25 +0900

    @RestController와 일반 Controller의 차이점 정리

--- src/main/java/com/example/firstproject/api/FirstApiController.java
+++ src/main/java/com/example/firstproject/api/FirstApiController.java
@@ -1,9 +1,10 @@
 package com.example.firstproject.api;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// 일반 controller와 차이점 : 일반 controller는 페이지가 반환됨
+// 하지만 RestController는 JSON 데이터가 나옴, 즉 반환 타입이 다름
 @RestController // RestApi용 컨트롤러! JSON을 반환
 public class FirstApiController {
 
