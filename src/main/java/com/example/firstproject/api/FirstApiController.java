2926f23720ca19c59a2b4a745745a2943aa6d2f1
문정훈 <mjh090801abc@dgsw.hs.kr>
2026-01-15 09:19:47 +0900

    Rest Api를 활용한 테스트 코드 작성

---
+++ src/main/java/com/example/firstproject/api/FirstApiController.java
@@ -0,0 +1,14 @@
+package com.example.firstproject.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // RestApi용 컨트롤러! JSON을 반환
+public class FirstApiController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "hello world!";
+    }
+}
