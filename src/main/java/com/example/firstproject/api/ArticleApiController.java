9c1a8ef60b5e283f9fc8c82ee3db235762385b33
문정훈 <mjh090801abc@dgsw.hs.kr>
2026-01-15 14:45:11 +0900

    트랜잭션을 수행하기 위한 service 호출 + 응답 코드

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -62,6 +62,14 @@ public class ArticleApiController {
 
     }
 
+    // 트랜잭션 -> 실패 -> 롤백!
+    @PostMapping("/api/transaction-test")
+    public ResponseEntity<List<Article>> transactionTest(@RequestBody List<ArticleForm> dtos) {
+        List<Article> createdList = articleService.createArticles(dtos);
+        return (createdList != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(createdList) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 
+    }
 
 }
