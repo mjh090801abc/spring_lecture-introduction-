c3af05f90834554685a209a08ac93e259ea3e918
문정훈 <mjh090801abc@dgsw.hs.kr>
2026-01-15 09:46:44 +0900

    PATCH의 기능을 활용하기 위한 코드 작성 + 먼저 수정용 엔티티 생성

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -3,11 +3,13 @@ package com.example.firstproject.api;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController// RestApi용 controller! 데이터(JSON)를 반환
 public class ArticleApiController {
 
@@ -35,8 +37,24 @@ public class ArticleApiController {
         return articleRepository.save(article);
     }
 
-
     // PATCH
+    @PatchMapping("/api/articles/{id}")
+    public Article update(@PathVariable Long id,
+                          @RequestBody ArticleForm dto) {
+
+        // 1. 수정용 엔티티 생성
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
+
+        // 2. 대상 엔티티를 조회
+
+        // 3. 잘못된 요청 처리(대상이 없거나, id가 다른 경우)
+
+        // 4. 업데이트 및 정상 응답(200)
+
+        return null;
+
+    }
 
     // DELETE
 }
