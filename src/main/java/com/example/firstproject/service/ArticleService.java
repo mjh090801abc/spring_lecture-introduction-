d7bb3ad97b55e098b23d0600865f75589e9769cd
문정훈 <mjh090801abc@dgsw.hs.kr>
2026-01-15 13:58:16 +0900

    Delete를 위한 로직 추가

--- src/main/java/com/example/firstproject/service/ArticleService.java
+++ src/main/java/com/example/firstproject/service/ArticleService.java
@@ -5,6 +5,8 @@ import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -38,5 +40,33 @@ public class ArticleService {
         // 1. 수정용 엔티티 생성
         Article article = dto.toEntity();
         log.info("id: {}, article: {}", id, article.toString());
+
+        // 2. 대상 엔티티 찾기
+        Article target = articleRepository.findById(id).orElse(null);
+
+        // 3. 잘못된 요청 처리(대상이 없거나, id가 다른 경우)
+        if (target == null || id != article.getId()) {
+            log.info("id: {}, article: {}", id, article.toString());
+            return null;
+        }
+
+        // 4. 업데이트 및 정상 응답(200)
+        target.patch(article);
+        Article updated = articleRepository.save(target);
+        return updated;
+
+    }
+
+    public Article delete(Long id) {
+        // 1. 대상 엔티티 찾기
+        Article target = articleRepository.findById(id).orElse(null);
+
+        // 2. 잘못된 요청 처리(대상 없는 경우)
+        if (target == null) {
+            return null;
+        }
+        // 3. 대상 삭제 후 응답 반환
+        articleRepository.delete(target);
+        return target;
     }
 }
