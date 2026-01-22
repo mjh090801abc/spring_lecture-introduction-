e24517f883937dcf07938286cbcf97abe642b94d
문정훈 <mjh090801abc@dgsw.hs.kr>
2026-01-22 11:38:40 +0900

    index 메소드에 관한 테스트 코드 작성

---
+++ src/test/java/com/example/firstproject/service/ArticleServiceTest.java
@@ -0,0 +1,34 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.entity.Article;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest // 해당 클래스는 스프링부트와 연동되어 테스팅됨
+class ArticleServiceTest {
+
+    @Autowired ArticleService articleService;
+
+    @Test // 해당 메소드가 테스트를 위한 코드
+    void index() {
+        // 예상
+        Article a = new Article(1L, "가가가가", "1111");
+        Article b = new Article(2L, "나나나나", "2222");
+        Article c = new Article(3L, "다다다다", "3333");
+        List<Article> expected = new ArrayList<Article>(Arrays.asList(a, b, c));
+
+        // 실제
+        List<Article> articles = articleService.index();
+
+        // 비교
+        assertEquals(expected, articles);
+
+    }
+}
